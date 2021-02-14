ca03fb112d6938d98b31364d78e361000c373540
Phong Luu <huynhpluu@gmail.com>
2021-02-15 00:21:21 +0700

    config: react-query

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,7 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const AppProviders = ({ children }: any) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
 export { AppProviders };
